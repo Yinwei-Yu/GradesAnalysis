e40ce9915577173e7ebccc01cdbbaae74b952492
白杨 <156503797+114514mbb@users.noreply.github.com>
2024-07-07 19:30:19 +0800

    修改了一下

--- Code/User_Student.py
+++ Code/User_Student.py
@@ -3,17 +3,17 @@
 class User_Student
 by 刘链凯
 '''
+import User
 import GradeManager
-from User import User
 class User_Student(User):
     def __init__(self, userName, passWord, ID):
         super().__init__(userName, passWord, ID)
-    def getSingleGrades(self)->list:
-        gradeManager = GradeManager.GradeManager(self.userName, self.ID, []);
-        return gradeManager.getSingleGrades();
+    def getSingleGrades(self, name, stuID, sub, grade)->bool:
+        gradeManager = GradeManager.GradeManager(self.userName, self.ID, [])
+        return gradeManager.inputSingle(name, stuID, grade)
     def GradesCheck(self, name, stuID, subject)->bool:
-        gradeManager = GradeManager.GradeManager(self.userName, self.ID, []);
-        return gradeManager.GradesCheck(name, stuID, subject);
+        gradeManager = GradeManager.GradeManager(self.userName, self.ID, [])
+        return gradeManager.inputGrades(1, name, stuID, subject)
 
 
 
