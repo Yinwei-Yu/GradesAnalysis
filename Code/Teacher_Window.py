1a90c945830f1c0dbd2f074d837b992b87aa8ec6
YuLongLiao <kuaijiu11@gmail.com>
2024-07-10 21:46:59 +0800

    做了教师查看成绩的详细界面,存在一点问题
    在点开新的窗口的时候,原来的窗口还在,并且还能用

--- Code/Teacher_Window.py
+++ Code/Teacher_Window.py
@@ -7,22 +7,55 @@ by 廖雨龙
 import tkinter as tk
 from tkinter import messagebox
 
-# 有一个新的界面,里面提供其他的成绩查询选项 未完成
-def disp_grades():
+# 实现查看成绩功能1 查看所有学生成绩
+def disp_all_grades():
+    print("我在测试")
+
+# 实现查看成绩功能2 显示总体成绩的分析
+def disp_all_analysis():
     pass
 
+# 实现查看成绩功能3 查看单个学生的成绩
+def disp_single_grade():
+    pass
+
+# 实现查看成绩功能4 查看单个学生的成绩分析报告
+def disp_single_analysis():
+    pass
+
+# 有一个新的界面,里面提供其他的成绩查询选项 未完成
+# 成绩展示的内置其他选项
+# 1. 查看所有学生成绩
+# 2. 所有学生成绩分析
+# 3. 查找个人成绩
+# 4. 个人成绩分析
+def disp_grades(tea_window):
+    tea_window.withdraw()
+    grade_window = tk.Toplevel(tea_window)
+    grade_window.title("查看成绩")
+    disp_all_grades_button = tk.Button(grade_window,text='显示所有学生成绩',command=disp_all_grades,width=30,height=3)
+    disp_all_grades_button.pack(pady=10)
+    disp_all_analysis_button = tk.Button(grade_window,text='总体成绩分析',command=disp_all_analysis,width=30,height=3)
+    disp_all_analysis_button.pack(pady=10)
+    disp_single_grade_button = tk.Button(grade_window,text='查看个人成绩',command=disp_single_grade,width=30,height=3)
+    disp_single_grade_button.pack(pady=10)
+    disp_single_analysis_button = tk.Button(grade_window,text='查看个人成绩分析',command=disp_single_analysis,width=30,height=3)
+    disp_single_analysis_button.pack(pady=10)
+    tea_window.deiconify()
 # 申请成绩复核函数,发送请求给admin
 def app_review():
     pass
 # 修改自己密码的函数  未实现
 def change_my_password():
-    pass
+    # 这是一个测试
+    print("This is a test")
+
 
 # 修改学生的密码的函数  未实现
 def change_stu_password():
     pass
 
-# 退出函数 返回到上一个界面
+# 退出函数 返回到主界面
 def log_out(tea_window,login_window,username_entry,password_entry):
     username_entry.delete(0,tk.END)
     password_entry.delete(0,tk.END)
@@ -38,7 +71,7 @@ def show_teacher_window(login_window,username_entry,password_entry):
     welcome_title = tk.Label(tea_window, text='你好!', font=('Arial', 14), width=20, height=2)
     welcome_title.pack(side='top')
     # 查看成绩->一个新的页面 包括各种成绩与分析
-    query_button = tk.Button(tea_window, text="查询成绩", command=disp_grades, width=30, height=3)
+    query_button = tk.Button(tea_window, text="查询成绩", command=lambda:disp_grades(tea_window), width=30, height=3)
     query_button.pack(pady=10)
     # 申请复核成绩按钮
     modify_button = tk.Button(tea_window, text="修改用户名密码", command=app_review, width=30, height=3)
