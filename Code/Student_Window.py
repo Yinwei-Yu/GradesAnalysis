96190c53b9ec2c591c2c2d6a52c943dce1b7a07d
YuLongLiao <kuaijiu11@gmail.com>
2024-07-10 11:47:03 +0800

    写了两个按钮

--- Code/Student_Window.py
+++ Code/Student_Window.py
@@ -6,21 +6,41 @@ by 刘杨健
 """
 
 import tkinter as tk
-from Login_Window import login_hit,username_entry
+from Login_Window import login_hit, username_entry
+from tkinter import messagebox
 
 # 如果登录窗口收到的信号为1，则关闭登录窗口，打开学生窗口
-if login_hit==3:
+if login_hit == 3:
     stu_window = tk.Tk()
     stu_window.title('学生窗口')
     stu_window.geometry('600x400')
 
+
+    # 成绩查询的函数 未完成
+    def query_scores():
+        # 这里要添加查询成绩的操作
+        messagebox.showinfo("查询成绩", "这里是查询成绩的结果")
+
+
+    # 修改用户名和密码的函数 未完成
+    def modify_credentials():
+        # 这里添加修改用户名和密码的操作
+        messagebox.showinfo("修改用户名密码", "修改完成")
+
+
     # 标题
-    welcome_title=tk.Label(stu_window ,text='你好!', font=('Arial', 14), width=20, height=2)
+    welcome_title = tk.Label(stu_window, text='你好!', font=('Arial', 14), width=20, height=2)
     welcome_title.pack(side='top')
 
-    #选项
-    #1、查询成绩
-    #2、修改用户名和密码
+    # 选项
+    # 1、查询成绩
+    # 2、修改用户名和密码
 
+    # 成绩查询按钮
+    query_button = tk.Button(stu_window, text="查询成绩", command=query_scores, width=30, height=3)
+    query_button.pack(pady=10)
+
+    # 修改用户名和密码的按钮
+    modify_button = tk.Button(stu_window, text="修改用户名密码", command=modify_credentials, width=30, height=3)
+    modify_button.pack(pady=10)
     stu_window.mainloop()
-
