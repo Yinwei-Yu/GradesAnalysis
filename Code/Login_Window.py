c9f44f51042b66d7b87adc4d45457d269231de43
gitliangjian <153593212+gitliangjian@users.noreply.github.com>
2024-07-11 21:40:44 +0800

    修改了登陆时的账号检测机制和界面跳转机制

--- Code/Login_Window.py
+++ Code/Login_Window.py
@@ -27,6 +27,7 @@ userid_label = tk.Label(login_window, text='学号/工号:', font=('Arial', 10))
 userid_label.place(x=135, y=100)
 password_label = tk.Label(login_window, text='密码:', font=('Arial', 10))
 password_label.place(x=135, y=140)
+
 # 设置两个var获取输入的学号和密码
 var_userid=tk.StringVar()
 var_password=tk.StringVar()
@@ -38,29 +39,73 @@ userid_entry.place(x=205, y=100)
 password_entry = tk.Entry(login_window, show='*', font=('Arial', 14),textvariable=var_password)
 password_entry.place(x=205, y=140)
 
-# 登录信号，初始为0，学生登录为1，教师登录为2，管理员登录为3
-login_hit = 0
 
+
+# 学号/工号和密码
+userid=""
+password=""
+
+# 登录提示标签
+login_var=tk.StringVar()
+login_label=tk.Label(login_window,textvariable=login_var)
+login_label.place(x=250,y=250)
+
+
+
+"""
+登录函数会检测学号和密码的输入是否为空，当二者均不为空时，获取到的学号和密码分别存放于userid 和 password中
+此时需要访问数据库查询对应账户，根据查询的结果进行页面跳转
+1、学号不存在，提示用户不存在
+2、学号存在，密码错误，提示用户重新输入密码，五次之后禁止再输入
+
+查询函数应返回identity信号，用于界面跳转
+"""
+
+# 身份信号，初始为0，学生登录为3，教师登录为2，管理员登录为1
+identity = 0
+
+# 标记用户是否存在
+is_exist=False
+
+# 查询函数，应访问数据库，返回查询结果,密码错误，identity=0，否则返回相应的identity值
+def find_user(userid,password):
+    is_exist=False # 返回是否存在
+    identity=0
+    return is_exist,identity
+
+# 记录密码错误次数
+fault_times=0
 def log_in():
-    global login_hit
-    if username_entry.get().strip()=="" and password_entry.get().strip()=="":
-        login_var.set('请输入用户名和密码')
-    elif username_entry.get().strip()=="":
-        login_var.set('请输入用户名')
-    elif password_entry.get().strip()=="":
+    global identity
+    if userid_entry.get()=="" and password_entry.get()=="":
+        login_var.set('请输入学号/工号和密码')
+    elif userid_entry.get()=="" and not password_entry.get()=="":
+        login_var.set('请输入学号/工号')
+    elif password_entry.get()=="":
         login_var.set('请输入密码')
     else:
+        userid=userid_entry.get()
+        password=password_entry.get()
+        # 调用查找函数
+        res_is_exist,res_identity=find_user(userid,password)
+        if res_is_exist==False:     # 用户不存在
+            login_var.set('用户不存在！')
+        else:
+            if identity==0:
+                fault_times+=1
+                login_var.set(f'密码错误，你还可以输入{5-fault_times}次')
+            elif identity==3:
+                login_window.withdraw()
+                show_student_window(login_window, username_entry, password_entry)
+            elif identity==2:
+                login_window.withdraw()
+                show_teacher_window(login_window, username_entry, password_entry)
+            else:   # identity==1
+                pass
         login_var.set('成功登录')
-        login_window.withdraw()
-        if identity_var.get()=='管理员':
-            login_hit=1
-        elif identity_var.get()=='老师':
-            login_hit=2
-            show_teacher_window(login_window,username_entry,password_entry)
-        else:                   # 学生
-            login_hit=3
-            show_student_window(login_window,username_entry,password_entry)
-    print(login_hit)
+
+    # 测试代码
+    # print(userid_entry.get(),password_entry.get())
 
 # 退出函数
 def log_out():
