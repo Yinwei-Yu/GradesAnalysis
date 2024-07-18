6250f55b089c50c160162a52e7faeac322e88166
YuLongLiao <kuaijiu11@gmail.com>
2024-07-18 10:13:25 +0800

    修改了管理员的修改成绩,现在点击完成之后可以清空输入并给出成功提示

--- Code/Administrator_Window.py
+++ Code/Administrator_Window.py
@@ -103,27 +103,36 @@ def view_application(tree, entry_id, combobox_course, label_student_name, label_
 # 完成成绩申请处理 删除Treeview中选中的项
 def finish_application(tree):
     selected_item = tree.selection()
-    print("testing")
-    print(tree.index(tree.selection()))
+    accountManager.deleteCheckApplication(tree.index(tree.selection()))
     if selected_item:
         tree.delete(selected_item[0])
 
 
 # 确认修改 确认并保存修改后的成绩
 # 还存在小问题,就是如果是自己输入的话会认为你没有输入
-def confirm_modification(entry_id, combobox_course, entry_new_grade):
+def confirm_modification(entry_id, combobox_course, entry_new_grade, label_student_name,label_original_grade):
     # 拿到输入的学号
     student_id = entry_id.get()
     # 拿到输入的新成绩
     new_grade = entry_new_grade.get()
     # 拿到修改的课程
     new_course = combobox_course.get()
+    # 拿到原来的成绩
+    original_grade = label_original_grade.cget("text")
+    # 拿到姓名
+    student_name = label_student_name.cget("text")
     print(student_id, new_grade, new_course)
     if accountManager.changeGrades(int(student_id), new_course, int(new_grade)):
-        messagebox.showinfo("成功", '成绩修改成功！')
+        success_message = f"修改成功！\n姓名: {student_name}\n学号:{student_id}\n原成绩:{original_grade}\n新成绩: {new_grade}"
+        messagebox.showinfo("成功", success_message)
     else:
         messagebox.showwarning("失败", '成绩修改失败！')
-
+    # 点击确认之后所有的输入框和提示什么的直接全清空
+    entry_id.delete(0, tk.END)
+    combobox_course.set("")
+    entry_new_grade.delete(0,tk.END)
+    label_student_name.config(text="")
+    label_original_grade.config(text="")
 
 # 不仅会更新下拉框中的选项,还会在学号无效或不在数据中的情况下清楚下拉框的当前值
 # 还要更新原成绩
@@ -428,9 +437,9 @@ def admin_disp_apps(admin_window):
     ttk.Label(right_frame, text="修改成绩:").grid(row=4, column=0, pady=5, sticky="e")
     entry_new_grade = ttk.Entry(right_frame)
     entry_new_grade.grid(row=4, column=1, pady=5)
-    # 修改完成后的确认按钮
+    # 修改完成后的确认按钮 # 确认之后直接将输入框之类的全部清空
     confirm_button = ttk.Button(right_frame, text="确认",
-                                command=lambda: confirm_modification(entry_id, combobox_course, entry_new_grade))
+                                command=lambda: confirm_modification(entry_id, combobox_course, entry_new_grade, label_student_name, label_original_grade))
     confirm_button.grid(row=5, column=1, pady=10, sticky="e")
     # 之后再在这里加个退出的按钮返回
     quit_button = ttk.Button(right_frame, text="退出",
